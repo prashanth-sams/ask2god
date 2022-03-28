d7dc40a858d39f091692c32d577ea594d2ef0b32
prashanth-sams <sams.prashanth@gmail.com>
2022-03-28 17:55:25 +0400

    fix all libs

--- jest.config.js
+++ jest.config.js
@@ -19,5 +19,6 @@ module.exports = {
     "moduleNameMapper": {
         "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
         "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
-    }
+    },
+    "testEnvironment": "./custom-jest-environment.js"
 };
--- src/components/timeline/timeline.component.spec.js
+++ src/components/timeline/timeline.component.spec.js
@@ -3,7 +3,7 @@ import Timeline from './timeline.component.js'
 import { mount, shallow } from 'enzyme';
 
 describe ('<Timeline />', () => {
-    it (`should contain the Timeline component`, () => {
+    it (`should contain the Timeline component`, async () => {
         const component = shallow(<Timeline />);
         expect((component).exists()).toBe(true);
     });
